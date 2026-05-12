745a09188593d60fb58f3f106b78cf5a51952759
nurhalizah <nurhalizah@appsku.co.id>
2026-05-12 20:05:56 +0700

    feat: login

--- src/sistempenjualan/SistemPenjualan.java
+++ src/sistempenjualan/SistemPenjualan.java
@@ -15,7 +15,9 @@ public class SistemPenjualan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(() -> {
+            new Login().setVisible(true);
+        });
     }
     
 }
